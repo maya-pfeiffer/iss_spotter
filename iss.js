e1416682c2c3f5e58741fff005b34a440eda9c94
Maya Pfeiffer <mayap93@hotmail.com>
2023-07-06 17:52:33 -0400

    fetchMyIp now fetches IP addresses

--- iss.js
+++ iss.js
@@ -1,1 +1,20 @@
+const request = require('request');
 
+const fetchMyIP = function(callback) {
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    } else {
+      const data = JSON.parse(body).data;
+      callback(null, data);
+    }
+  }
+)};
+
+
+module.exports = { fetchMyIP};
