efc868c9e4f2cc1e7a3023432ae0c47a2919a6f5
Maya Pfeiffer <mayap93@hotmail.com>
2023-07-06 21:04:28 -0400

    Complete ISS acitvity

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-//const { fetchMyIP } = require('./iss');
+const { nextISSTimesForMyLocations } = require('./iss');
 
-fetchMyIP((error, ip) => {
-if (error) {
-  console.log("It didn't work!", error);
-  return;
-}
-console.log('It worked! Returned IP:', ip)
+const printTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocations((error, times) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  printTimes(times);
 });
